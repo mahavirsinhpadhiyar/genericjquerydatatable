2b4014c5edb9ef7838b546906b74e858ff68fa1e
alpeshkalena <alpeshkalena@outlook.com>
2019-01-06 19:46:22 +0530

    New dynamic parameter changes by mahavirsinh padhiyar

--- DataTableExample/Scripts/Datatable/CommonFunction.js
+++ DataTableExample/Scripts/Datatable/CommonFunction.js
@@ -1,14 +1,49 @@
-﻿var table;
-var CommonFunction = function (tableName, serverSide, bSortable, ajaxURL, dynamicColumns) {
-    table = $("#" + tableName).DataTable({
+﻿var custDatatable;
+var ExtRequestType = "GET";
+var ExtPageLength = "10";
+var ExtLengthMenu = [10, 25, 50];
+
+function Ext1Datatable(tableObj, url, requestType, dynamicColumns) {
+
+    custDatatable = $("#" + tableObj).DataTable({
+        "ajax": {
+            "url": url,
+            "type": ExtRequestType,
+            "data": function (result) {
+                result.search = $("#" + tableObj + "_filter input[type='search']").val();
+            },
+        },
+
+        "dataSrc": function (response) {
+            return response.data;
+        },
+        "columns": dynamicColumns[0]
+    });
+}
+
+function Ext2Datatable(tableObj, serverSide, bSortable, url, requestType, pageLength, lengthMenu, dynamicColumns) {
+
+    if (!requestType == "") {
+        ExtRequestType = requestType;
+    }
+
+    if (!pageLength == "") {
+        ExtPageLength = pageLength;
+    }
+
+    if (!lengthMenu == "") {
+        ExtLengthMenu = lengthMenu;
+    }
+
+    custDatatable = $("#" + tableObj).DataTable({
         "processing": true,
         "serverSide": serverSide,
         "bSortable": bSortable,
         "ajax": {
-            "url": ajaxURL,
-            "type": "POST",
+            "url": url,
+            "type": ExtRequestType,
             "data": function (result) {
-                result.search = $("#" + tableName + "_filter input[type='search']").val();
+                result.search = $("#" + tableObj + "_filter input[type='search']").val();
             },
         },
 
@@ -16,13 +51,10 @@ var CommonFunction = function (tableName, serverSide, bSortable, ajaxURL, dynami
             return response.data;
         },
         "columns": dynamicColumns[0],
-        "pageLength": 10,
-        "LengthMenu": [10, 25, 50],
+        "pageLength": ExtPageLength,
+        "LengthMenu": ExtLengthMenu,
     });
-
 }
-
-
 
 var ColumnNames = function (columns) {
     var obj = [];
